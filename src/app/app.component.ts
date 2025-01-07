80c154b25601d533c57e4401d6ec9fba7f28e710
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-01-07 19:10:20 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [SidebarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sidebar';
+}
