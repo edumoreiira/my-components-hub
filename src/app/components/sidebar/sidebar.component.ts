84f6b865ff760d4c23b1932950b0006bbe3cfd79
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-01-09 22:25:22 -0300

    feat: add collapse navbar functionality

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  isCollapsed = false;
 
+  collapseSidebar() {
+    this.isCollapsed = true;
+  }
+  
+  expandSidebar() {
+    this.isCollapsed = false;
+  }
+
+  toggleSidebar() {
+    this.isCollapsed ? this.expandSidebar() : this.collapseSidebar();
+  }
 }
